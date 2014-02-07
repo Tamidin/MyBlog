feaab8d6392260b8732639d45d02ccaadeecda0a
Tamidin <taubm@yandex.ru>
2014-02-07 21:53:13 +0900

    Слегка подрихтовали скелет функциональности

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , posts = require('./routes/posts.js');
   var http = require('http');
-var path = require('path');
+  var moment = require("moment");
+  var path = require('path');
   var mongoskin = require('mongoskin');
   var db = mongoskin.db('mongodb://localhost:27017/blog?auto_reconnect', {safe:true});
 
@@ -42,6 +43,7 @@ app.use(function(req, res, next) {
 
 
 // Routes
+//Authorization
 app.post('/auth', function(req, res){
   req.db.users.findOne({login: req.body.user.login, password: req.body.user.pass}, function(err, user){
     if (user) {
@@ -60,6 +62,7 @@ app.get('/auth2', loadUser, function(req, res) {
   res.redirect('/');
 });
 
+//Registration
 app.get('/registration', function(req, res){
   res.render('registration.jade');
 });
@@ -77,44 +80,74 @@ app.post('/registration', function(req, res) {
     }
   });
 });
-  
+
+//index
 app.get('/', function(req, res){
-  req.db.posts.find().toArray(function(err, posts){
+  req.db.posts.find().sort({$natural: -1}).toArray(function(err, posts){
       if (req.session.user_id) {req.db.users.findOne({_id: db.ObjectID.createFromHexString(req.session.user_id)}, function(err, user) {
         if (user) {
-          res.render('blog.jade', {user: user, posts: posts || []});
+          res.render('blog.jade', {moment: moment, user: user, posts: posts || []});
         } 
       });
     } else {
-      res.render('blog.jade', {posts: posts || []});
+      res.render('blog.jade', {moment: moment, posts: posts || []});
     }
-    
     //console.log(posts);
   });
 });
 
-app.get('/post:id.:format?', function(req, res){
+//Post
+app.get('/post/id:id.:format?', function(req, res){
   req.db.posts.findOne({_id: db.ObjectID.createFromHexString(req.params.id)}, function(err, post){
-    var comms = req.db.comms.findOne({}, function(err, result){return result;});
-    console.log(comms);
-      if (req.session.user_id) {req.db.users.findOne({_id: db.ObjectID.createFromHexString(req.session.user_id)}, function(err, user) {
-        if (user) {
-          res.render('post.jade', {user: user, post: post, comms: comms});
-        }
-      });
+    req.db.comms.find({post_id: req.params.id}).toArray(function(err, comms){
+      if (req.session.user_id) {
+        req.db.users.findOne({_id: db.ObjectID.createFromHexString(req.session.user_id)}, function(err, user) {
+          if (user) {
+            res.render('post.jade', {moment: moment, user: user, post: post, comms: comms});
+          };
+        });
       } else {
-      res.render('post.jade', {post: post});
-    }
-    console.log(req.params.id);
-    console.log(post);
+        res.render('post.jade', {moment: moment, post: post, comms: comms});
+      }
+    });
   });
 });
 
-app.get('/add_post', loadUser, function(req, res){
+app.put('/post/id:id.:format?', function(req, res) {
+    req.db.posts.update({_id: db.ObjectID.createFromHexString(req.params.id)}, {$set: {
+      body: req.body.post.body,
+      title: req.body.post.title,
+      m_date: new Date()
+    }
+    }, function(err, result) {
+    if (err) throw err;
+      res.redirect('post/id'+req.body.post.id);
+    });
+});
+
+app.delete('/post/id:id.:format?', function(req, res) {
+  req.db.comms.remove({post_id: req.params.id}, function(err, result) {
+    if (err) throw err;
+    });
+    req.db.posts.remove({_id: db.ObjectID.createFromHexString(req.params.id)}, function(err, result) {
+    if (err) throw err;
+    res.redirect('/');
+    });
+});
+
+//Edit post
+app.get('/post/edit/id:id.:format?', loadUser, function(req, res){
+  req.db.posts.findOne({_id: db.ObjectID.createFromHexString(req.params.id)}, function(err, post){
+    res.render('edit_post.jade', {post: post});
+  });
+});
+
+//New post
+app.get('/post/new', loadUser, function(req, res){
   res.render('new_post')
 });
 
-app.post('/add_post', function(req, res) {
+app.post('/post/new', function(req, res) {
   // Находим документ
   console.log(req.body.post.title);
     db.collection('posts').insert({
@@ -123,22 +156,33 @@ app.post('/add_post', function(req, res) {
     cr_date: new Date()
   }, function(err,result) {
         if (err) throw err;
-    if (result) console.log();
+    res.redirect('/');
   });
 });
 
-app.post('/add_comm:id.:format?', function(req, res) {
-  // Находим документ
+//Comment
+app.delete('/comm/:id.:format?', function(req, res) {
+  req.db.comms.remove({_id: db.ObjectID.createFromHexString(req.params.id)}, function(err, result) {
+    if (err) throw err;
+    res.redirect('post/id'+req.body.post.id);
+    });
+});
+
+app.post('/comm/:id.:format?', function(req, res) {
+  req.db.users.findOne({_id: db.ObjectID.createFromHexString(req.session.user_id)}, function(err, user) {
     db.collection('comms').insert({
-    post_id: req.params.id,
-    author: req.db.users.findOne({_id: db.ObjectID.createFromHexString(req.session.user_id)}).login,
-    body: req.body.comm.body,
-    cr_date: new Date()
-  }, function(err,result) {
-        if (err) throw err;
-    if (result) console.log();
+      post_id: req.body.post.id,
+      author: user.login,
+      body: req.body.comm.body,
+      cr_date: new Date()
+    }, function(err, result) {
+      if (err) throw err;
+      res.redirect('post/id'+req.body.post.id);
+    });
   });
 });
+
+
 
 function loadUser(req, res, next) {
   if (req.session.user_id) {
